f9333bb6a9b647b656d840b9fcfbf5918840c6df
ericchase <github@eric.addymail.com>
2024-08-09 18:57:38 -0500

    hopefully last version
    Signed-off-by: ericchase <github@eric.addymail.com>

--- Library/src/lib/external/Platform/Web/AnchorDownloader.ts
+++ Library/src/lib/external/Platform/Web/AnchorDownloader.ts
@@ -1,27 +1,25 @@
-export class AnchorDownloader {
-  static download(data: string, filename: string) {
-    const a = document.createElement('a');
-    a.setAttribute('href', data);
-    a.setAttribute('download', filename);
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-  }
+export function anchor_downloader(data: string, filename: string) {
+  const a = document.createElement('a');
+  a.setAttribute('href', data);
+  a.setAttribute('download', filename);
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
 
-  static Blob(blob: Blob, filename: string) {
-    AnchorDownloader.download(URL.createObjectURL(blob), filename);
-  }
-  static Url(url: string, filename: string) {
-    AnchorDownloader.download(url, filename);
-  }
+export function SaveBlob(blob: Blob, filename: string) {
+  anchor_downloader(URL.createObjectURL(blob), filename);
+}
+export function SaveUrl(url: string, filename: string) {
+  anchor_downloader(url, filename);
+}
 
-  static Buffer(buffer: ArrayBuffer, filename: string) {
-    AnchorDownloader.Blob(new Blob([buffer], { type: 'application/octet-stream;charset=utf-8' }), filename);
-  }
-  static JSON(json: string, filename: string) {
-    AnchorDownloader.Blob(new Blob([json], { type: 'application/json;charset=utf-8' }), filename);
-  }
-  static Text(text: string, filename: string) {
-    AnchorDownloader.Blob(new Blob([text], { type: 'text/plain;charset=utf-8' }), filename);
-  }
+export function SaveBuffer(buffer: ArrayBuffer, filename: string) {
+  SaveBlob(new Blob([buffer], { type: 'application/octet-stream;charset=utf-8' }), filename);
+}
+export function SaveJSON(json: string, filename: string) {
+  SaveBlob(new Blob([json], { type: 'application/json;charset=utf-8' }), filename);
+}
+export function SaveText(text: string, filename: string) {
+  SaveBlob(new Blob([text], { type: 'text/plain;charset=utf-8' }), filename);
 }
