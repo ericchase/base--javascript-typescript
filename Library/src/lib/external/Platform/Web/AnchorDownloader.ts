8d9fb5d370dbb1c5391239942064eeaa193c4944
ericchase <github@eric.addymail.com>
2024-08-09 18:33:41 -0500

    fix up a tag downloader
    Signed-off-by: ericchase <github@eric.addymail.com>

--- Library/src/lib/external/Platform/Web/AnchorDownloader.ts
+++ Library/src/lib/external/Platform/Web/AnchorDownloader.ts
@@ -1,16 +1,27 @@
 export class AnchorDownloader {
-  static download(data: string, filename: string, mimetype: string) {
+  static download(data: string, filename: string) {
     const a = document.createElement('a');
-    a.setAttribute('href', `data:${mimetype};charset=utf-8,${encodeURIComponent(data)}`);
+    a.setAttribute('href', data);
     a.setAttribute('download', filename);
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
   }
-  static JSON(data: string, filename: string) {
-    AnchorDownloader.download(data, filename, 'application/json');
+
+  static Blob(blob: Blob, filename: string) {
+    AnchorDownloader.download(URL.createObjectURL(blob), filename);
   }
-  static Text(data: string, filename: string) {
-    AnchorDownloader.download(data, filename, 'text/plain');
+  static Url(url: string, filename: string) {
+    AnchorDownloader.download(url, filename);
+  }
+
+  static Buffer(buffer: ArrayBuffer, filename: string) {
+    AnchorDownloader.Blob(new Blob([buffer], { type: 'application/octet-stream;charset=utf-8' }), filename);
+  }
+  static JSON(json: string, filename: string) {
+    AnchorDownloader.Blob(new Blob([json], { type: 'application/json;charset=utf-8' }), filename);
+  }
+  static Text(text: string, filename: string) {
+    AnchorDownloader.Blob(new Blob([text], { type: 'text/plain;charset=utf-8' }), filename);
   }
 }
