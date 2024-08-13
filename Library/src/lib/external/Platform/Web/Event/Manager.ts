e85e1b927fe78d3f6d0a28602e5c9b28bb404c92
ericchase <github@eric.addymail.com>
2024-08-12 21:47:36 -0500

    up
    Signed-off-by: ericchase <github@eric.addymail.com>

--- Library/src/lib/external/Platform/Web/Event/Manager.ts
+++ Library/src/lib/external/Platform/Web/Event/Manager.ts
@@ -1,13 +1,12 @@
-type EventType = string;
-interface EventSource {
-  addEventListener: (type: any, listener: (event: any) => any, useCapture: any) => void;
-  removeEventListener: (type: any, listener: (event: any) => any, useCapture: any) => void;
-}
-type ListenerCallback = (type: EventType, event: Event, options: EventOptions) => { remove: boolean } | void;
+type ListenerCallback = (type: string, event: Event, options: EventOptions) => { remove: boolean } | void;
 interface EventOptions {
   direction?: 'bubble' | 'capture';
   once?: boolean;
   passive?: boolean;
+}
+interface EventSource {
+  addEventListener: (type: any, listener: (event: any) => any, useCapture: any) => void;
+  removeEventListener: (type: any, listener: (event: any) => any, useCapture: any) => void;
 }
 
 class ECEventManager {
@@ -28,26 +27,22 @@ class ECEventManager {
    * Create one or more event listeners for each event type in `types`, for each event source in `sources`, for each listener callback in `callbacks`, and for each event options in `options`. (Passing multiple values to any parameter will result in the creation of multiple event listeners.)
    *
    * @parameter options: if `{}`, `[]`, or undefined, the default event options `{ direction: 'bubble', once: false, passive: false }` will be used for each event listener created.
-   * @returns a function for removing the event listeners created with this specific call to the `add` method. Event listeners associated with the combination of properties passed to `removeOptions` will be removed. Passing `[]` to a property of `removeOptions` will remove all the listeners associated with that specific property in combination with the other properties. Ultimately, passing `[]` to all properties (or leaving `removeOptions` undefined) will remove every listener associated with this call.
+   * @returns a function for removing the event listeners created with this specific call to the `add` method. Event listeners associated with the combination of properties passed to `removeParams` will be removed. Passing `[]` to a property of `removeParams` will remove all the listeners associated with that specific property in combination with the other properties. Ultimately, passing `[]` to all properties (or leaving `removeParams` undefined) will remove every listener associated with this call.
    */
-  add(
-    types: EventType | EventType[], //
-    sources: EventSource | EventSource[],
-    callbacks: ListenerCallback | ListenerCallback[],
-    options: EventOptions | EventOptions[] = {},
-  ) {
+  add(addParams: {
+    types: string | string[];
+    sources: EventSource | EventSource[];
+    callbacks: ListenerCallback | ListenerCallback[]; //
+    options?: EventOptions | EventOptions[];
+  }) {
     // TODO
     return {
       remove: (
-        removeOptions: {
-          types: EventType | EventType[];
+        removeParams: {
+          types: string | string[];
           sources: EventSource | EventSource[];
-          options: EventOptions | EventOptions[];
-        } = {
-          types: [],
-          sources: [],
-          options: [],
-        },
+          options?: EventOptions | EventOptions[];
+        } = { types: [], sources: [], options: [] },
       ) => {
         // TODO
       },
@@ -57,7 +52,7 @@ class ECEventManager {
    * Remove and re-add the listeners associated with the parameters provided. The listener callbacks will not be destroyed; they will be continue to exist with their associated listeners. This is an advanced operation for specific situations. Normally, there is no need to call `refresh`.
    */
   refresh(
-    types: EventType | EventType[], //
+    types: string | string[], //
     sources: EventSource | EventSource[],
     options: EventOptions | EventOptions[] = {},
   ) {
@@ -66,6 +61,3 @@ class ECEventManager {
 }
 
 export const EventManager = ECEventManager.GetGlobalManager();
-
-const { remove } = EventManager.add('click', window, () => {});
-remove();
